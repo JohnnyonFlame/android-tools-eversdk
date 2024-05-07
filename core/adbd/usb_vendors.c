fbf925497747b4b69a90115f1d0c2df6cdee9ffd
Johnny on Flame <johnnyonflame@hotmail.com>
2024-05-07 04:06:26 +0000

    Applied debian patches.

--- core/adbd/usb_vendors.c
+++ core/adbd/usb_vendors.c
@@ -131,10 +131,6 @@
 #define VENDOR_ID_KOBO          0x2237
 // Teleepoch's USB Vendor ID
 #define VENDOR_ID_TELEEPOCH     0x2340
-// Meizu's USB Vendor ID
-#define VENDOR_ID_MEIZU         0x2a45
-// BQ's USB Vendor ID
-#define VENDOR_ID_BQ            0x2a47
 
 
 /** built-in vendor list */
@@ -187,8 +183,6 @@ int builtInVendorIds[] = {
     VENDOR_ID_YULONG_COOLPAD,
     VENDOR_ID_KOBO,
     VENDOR_ID_TELEEPOCH,
-    VENDOR_ID_MEIZU,
-    VENDOR_ID_BQ,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
